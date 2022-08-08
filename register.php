44489e8736d9edcd70ef42a2b333edc2a2b21a66
Raymond Lind <73867586+Raymond-Lind@users.noreply.github.com>
2022-08-08 17:12:36 -0400

    Update register.php

--- register.php
+++ register.php
@@ -25,7 +25,8 @@ if( isset( $_POST[ 'Login' ] ) ) {
 	$pass = stripslashes( $pass );
 	$pass = ((isset($GLOBALS["___mysqli_ston"]) && is_object($GLOBALS["___mysqli_ston"])) ? mysqli_real_escape_string($GLOBALS["___mysqli_ston"],  $pass ) : ((trigger_error("[MySQLConverterToo] Fix the mysql_escape_string() call! This code does not work.", E_USER_ERROR)) ? "" : ""));
 	$pass = md5( $pass );
-
+	
+	$query  = "CREATE USER '$user'@'localhost' IDENTIFIED BY '$pass';";
 	$query = ("SELECT table_schema, table_name, create_time
 				FROM information_schema.tables
 				WHERE table_schema='{$_DVWA['db_database']}' AND table_name='users'
