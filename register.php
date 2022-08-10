20f347380035a93ce51431be175021076eef5d81
Raymond Lind <73867586+Raymond-Lind@users.noreply.github.com>
2022-08-10 15:34:11 -0400

    Update register.php

--- register.php
+++ register.php
@@ -39,6 +39,7 @@ if( isset( $_POST[ 'Register' ] ) ) {
 	$user = $_POST[ 'username' ];
 	$firstname = $_POST[ 'first' ];
 	$lastname = $_POST[ 'last' ];
+	$lastname = $_POST[ 'isEmployee' ];
 	if ($password != $confirmpassword) {
 		echo "Passwords do not match, please try again!";
 	} else {
@@ -51,7 +52,7 @@ if( isset( $_POST[ 'Register' ] ) ) {
 			$userquery = ("SELECT * FROM dvwa.users;");
 			$userresult = @mysqli_query($GLOBALS["___mysqli_ston"],  $userquery );
 			$count = mysqli_num_rows( $userresult ) + 1;
-			$query  = "INSERT INTO dvwa.users (user_id, first_name, last_name, user, password, avatar, failed_login) values ('$count', '$firstname', '$lastname', '$user', '$pass', '/hackable/users/admin.jpg', '0');";
+			$query  = "INSERT INTO dvwa.users (user_id, first_name, last_name, user, password, avatar, failed_login, employee) values ('$count', '$firstname', '$lastname', '$user', '$pass', '/hackable/users/admin.jpg', '0');";
 			$result = @mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '.<br />Try <a href="setup.php">installing again</a>.</pre>' );
 			if( $result && mysqli_num_rows( $result ) == 1 ) {    // Login Successful...
 				dvwaMessagePush( "You have logged in as '{$user}'" );
