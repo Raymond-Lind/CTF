b5962b010429ee2e5cea77336da5a4161cfe7e19
Raymond Lind <73867586+Raymond-Lind@users.noreply.github.com>
2022-08-10 13:08:22 -0400

    Update register.php

--- register.php
+++ register.php
@@ -35,9 +35,10 @@ if( isset( $_POST[ 'Register' ] ) ) {
 		dvwaMessagePush( "First time using DVWA.<br />Need to run 'setup.php'." );
 		dvwaRedirect( DVWA_WEB_PAGE_TO_ROOT . 'setup.php' );
 	}
-	$userquery = ("SELECT COUNT(*) FROM dvwa.users;");
+	$userquery = ("SELECT * FROM dvwa.users;");
 	$userresult = @mysqli_query($GLOBALS["___mysqli_ston"],  $userquery );
-	$count = $userresult + 1;
+	$count = mysqli_num_rows( $userresult ) + 1;
+	echo "<h1>$userresult</h1>"
 	$query  = "INSERT INTO dvwa.users (user_id, first_name, last_name, user, password) values ('$count', 'Alex', 'Lind', '$user', '$pass');";
 	$result = @mysqli_query($GLOBALS["___mysqli_ston"],  $query ) or die( '<pre>' . ((is_object($GLOBALS["___mysqli_ston"])) ? mysqli_error($GLOBALS["___mysqli_ston"]) : (($___mysqli_res = mysqli_connect_error()) ? $___mysqli_res : false)) . '.<br />Try <a href="setup.php">installing again</a>.</pre>' );
 	if( $result && mysqli_num_rows( $result ) == 1 ) {    // Login Successful...
